64456923e22d1f55af558191a802b283fb57f5d3
Gus Vc <guzz.vc@gmail.com>
2025-07-05 13:18:49 -0600

    feat: Products controller added

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //https://localhost:5001/api/products
+    [ApiController]
+    public class ProductsController (StoreContext context) : ControllerBase
+    {         
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
+
+            if (!products.Any())return NoContent();
+            
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id) 
+        {
+            var product = context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null) return NotFound();
+            
+            return Ok(product);
+        }
+    }
+}
