f8f7439beb6e59c4e2f87c8b745d594194da8239
Gus Vc <guzz.vc@gmail.com>
2025-07-05 12:47:50 -0600

    db initializer dummy data added

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddSwaggerGen();
@@ -26,5 +36,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+DbInitializer.InitDb(app);
 
 app.Run();
